e10fad972982a84ed97bf131acaeda1de4a7fba8
josh-ramer <38678260+josh-ramer@users.noreply.github.com>
2020-11-10 15:37:15 +0000

    navbar in app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,81 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import MainCard from './containers/MainCard';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import HomeIcon from '@material-ui/icons/Home';
+import DomainContainer from './containers/DomainContainer';
+import ChallengeContainer from './containers/ChallengeContainer';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    },
+}));
 
 function App() {
+    const classes = useStyles();
+    const Linker = props => <Link to="/" {...props}/>
     return (
         <div>
+            <div className={classes.root}>
+                <AppBar style={{ background: '#282c34'}} position="static">
+                    <Toolbar>
+                        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography variant="h6" className={classes.title}>
+                        </Typography>
+                        <Router>
+                            <Button color="inherit" component={Linker}>
+                                <HomeIcon />
+                            </Button>
+                        </Router>
+                    </Toolbar>
+                </AppBar>
+            </div>
             <header className="App-header">
                 <h1>
                     RoboRank
                 </h1>
             </header>
 
-            <MainCard />
+            <div className="App"
+                style={{
+                    display: "block",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    height: "100vh"
+                }}
+            >
+                <Router>
+                    <Switch>
+                        <Route path="/domains">
+                            <DomainContainer />
+                        </Route>
+                        <Route path="/">
+                            <DomainContainer />
+                        </Route>
+                    </Switch>
+                </Router>
+            </div>
 
             <footer className="App-footer">
                 <img src={logo} className="App-logo" alt="logo" />
--- src/containers/DomainContainer.js
+++ src/containers/DomainContainer.js
@@ -1,20 +1,54 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+// import Paper from '@material-ui/core/Paper';
 import Domain from "../components/Domain"
 
-class DomainContainer extends Component {
-    render() {
-        return (
-            <div
-                style={{
-                    display: "block",
-                    justifySelf: "center",
-                    alignSelf: "center",
-                    maxHeight: "450px",
-                    maxWidth: "450px"
-                }}
-            >
-                <Domain/>
-            </div>
-        );
-    }
-} export default DomainContainer;
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    control: {
+        padding: theme.spacing(2),
+    },
+}));
+
+export default function DomainContainer() {
+    const classes = useStyles();
+    const [spacing] = React.useState(2);
+
+    const domains = [
+        {
+            name: "Robot Modeling",
+            description: "The details..."
+        },
+        {
+            name: "Rigid-Body Motion"
+        },
+        {
+            name: "Forward Kinematics"
+        },
+        {
+            name: "Inverse Kinematics"
+        },
+        {
+            name: "Dynamics"
+        },
+        {
+            name: "Trajectory Generation"
+        },
+    ]
+    return (
+        <Grid container className={classes.root} spacing={3}>
+            <Grid item xs={12}>
+                <Grid container justify="center" spacing={spacing}>
+                    {domains.map((domain) => (
+                        <Grid key={domain.name} item>
+                            <Domain domain={domain} />
+                        </Grid>
+                    ))}
+                </Grid>
+            </Grid>
+        </Grid>
+    )
+}
